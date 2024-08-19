389d5211e70100e270f0707e089f8d8e7425fb10
Pablo Bozzolo <boctulus@gmail.com>
2024-08-19 13:13:29 +0800

    .

--- assets/js/menu_items.js
+++ assets/js/menu_items.js
@@ -2,11 +2,11 @@ const menu_items = {
     admin:  [
         { text: "Sign out", 
           atts: {
-            "id": "sign_our",
-            "click": "my_callback(this);"
+          "id": "sign_out",
+          "click": "logout();"
           },
           icon: "log-out",
-          secondary_icon: "<span onclick=\"event.stopPropagation(); alert('¡Hola desde el botón!')\" data-feather='chevrons-right' style='position: absolute; right: 10px;'></span>"
+          secondary_icon: "<span onclick=\"event.stopPropagation(); alert('Toggling sidebar')\" data-feather='chevrons-right' style='position: absolute; right: 10px;'></span>"
         },
         { 
           text: "Exam list", 
